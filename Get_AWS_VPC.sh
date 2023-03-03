522ceedbe8d93ba84fc8e114ec7d5230b3a79197
ramkopparthi <ramkopparthi210@gmail.com>
2023-03-03 17:02:16 +0530

    initial commit

--- Get_AWS_VPC.sh
+++ Get_AWS_VPC.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-REGION=
-aws ec2 describe-vpcs --region $REGION
+#REGION='us-east-1'
+REGION=$2
+#FORMATE='table'
+FORMATE=$1
+echo "first line"
+aws ec2 describe-vpcs --region $REGION --output $FORMATE
